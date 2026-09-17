3ad31beec883a8acd1d29baab32bbce5d0408a72
Williamcmarques8-ctrl <william.c.marques8@gmail.com>
2026-09-17 13:22:44 -0300

    algumas alterações

---
+++ src/App.jsx
@@ -0,0 +1,133 @@
+import {useState} from "react"
+import {fileTypeFromBlob} from "file-type";
+
+export default function XanXan(){
+
+    const [texto, setTexto] = useState("")
+    const [arquivo, setArquivo] = useState(null)
+    const [textocorrigido, setTextocorrigido] = useState("")
+    const [loading, setLoading] = useState("")
+    const [formatoarquivo, setFormatoarquivo] = useState("")
+    const [antigonome, setAntigonome] = useState("")
+    const [nome, setNome] = useState("")
+    const [textocerto, setTextocerto] = useState(false)
+
+    const API_KEY = import.meta.env.VITE_GROQ_API_KEY
+    
+
+    function aocolocararquivo(evento){
+        setArquivo(evento.target.files[0])
+        indentificarformatoarquivo(evento.target.files[0])
+        setAntigonome(evento.target.files[0].name)  
+    }
+
+    function aodigitar(evento){
+        setTexto(evento.target.value)
+    }
+
+    function limpararquivo() {
+        setArquivo(null)
+    }
+        
+    function aoclickar(){
+        setLoading(true)
+        identificarFormato(texto)
+        nomearquivo(antigonome)
+    }
+    
+    async function indentificarformatoarquivo(arquivo){
+        const retornoFileType = await fileTypeFromBlob(arquivo)
+        if (retornoFileType === undefined) {
+            setFormatoarquivo("invalido")
+            limpararquivo()
+        }else{
+            setFormatoarquivo(retornoFileType.ext)
+    
+        }
+    }
+
+    function nomearquivo(nome) {
+        var nomesplit = nome.split(".")
+        nomesplit.pop()
+        var nomejoin = nomesplit.join(" ")
+        setNome(nomejoin)
+    }   
+
+    function baixararquivo(){
+        const url = URL.createObjectURL(arquivo)
+        const link = document.createElement("a")
+        link.href = url
+        link.download = `${nome}.${textocorrigido}`
+        link.click()
+        URL.revokeObjectURL(url)
+    }
+
+    async function identificarFormato(texto) {
+        const resposta = await fetch("https://api.groq.com/openai/v1/chat/completions", 
+            {
+            method: "POST",
+            headers: {
+                    "Content-Type": "application/json",
+                    
+                    "Authorization": `Bearer ${API_KEY}`
+                },
+                body: JSON.stringify({
+                    
+                    model: "openai/gpt-oss-120b",
+                    messages: [
+                        {
+                            role: "user",
+                            content: `O usuário digitou o seguinte texto tentando indicar um formato de arquivo: "${texto}".
+                            Responda APENAS com a extensão do formato de arquivo, em minúsculas, sem aspas, sem pontuação, sem explicação nenhuma antes ou depois.  
+                            Exemplos de resposta correta: pdf
+                            jpg
+                            docx
+                            Se possivel indentificar erro de digitação, corrigir, exemplo: pdff, world
+                            Se não for possível identificar um formato de arquivo válido, responda apenas: invalido`
+                        }
+                    ],
+                    temperature: 0.1 
+                })
+            })
+        const dados = await resposta.json()
+        const textoResposta = dados.choices[0].message.content
+        setTextocorrigido(textoResposta.trim())
+        setLoading(false)
+    }
+
+return(
+    
+<div>
+
+    {formatoarquivo === "invalido" && 
+    <div>
+        <h2>Formato do arquivo invalido</h2>
+    </div>
+    }
+
+    {!arquivo && !textocorrigido && !loading &&
+        <input type="file" onChange={aocolocararquivo}/>
+    }
+    
+    {formatoarquivo != "invalido" && arquivo && !textocorrigido && !loading && !textocerto &&
+        <div>
+            <h2>Para qual arquivo deseja tranformar?</h2> 
+            <input type="text" onChange={aodigitar}/>
+            <button onClick={aoclickar}>Enviar</button>
+        </div>  
+    }
+    
+    {loading && 
+        <h2>gerando arquivo...</h2>
+    }
+
+    {textocorrigido && textocerto &&
+        <div>
+            <h2>Aqui esta, o seu arquivo foi de {formatoarquivo} para {textocorrigido}</h2>
+            <button onClick={baixararquivo}>baixar arquivo</button>
+        </div>
+    }
+</div>
+
+)
+}
