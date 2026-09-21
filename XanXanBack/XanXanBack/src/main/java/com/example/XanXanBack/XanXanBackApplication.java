4ee80c7cbd9f0d227742058cef528ce06b7032f6
Williamcmarques8-ctrl <william.c.marques8@gmail.com>
2026-09-21 14:11:01 -0300

    Mudanca de pasta

---
+++ XanXanBack/XanXanBack/src/main/java/com/example/XanXanBack/XanXanBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.XanXanBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XanXanBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XanXanBackApplication.class, args);
+	}
+
+}
---
+++ XanXanBack/XanXanBack/src/test/java/com/example/XanXanBack/XanXanBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.XanXanBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XanXanBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
