4d86df9793fb93bd6eb075426db5e3e5f3233fe9
Williamcmarques8-ctrl <william.c.marques8@gmail.com>
2026-09-22 15:11:49 -0300

    criei rota

--- XanXanBack/src/main/java/com/example/XanXanBack/controller/XanXanBackController.java
+++ XanXanBack/src/main/java/com/example/XanXanBack/controller/XanXanBackController.java
@@ -2,12 +2,15 @@ package com.example.XanXanBack.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;   
 
 
 @RestController
 public class XanXanBackController{
-    @GetMapping("/teste")
-    public String teste(){
-        return "teste";
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file){
+        return file.getOriginalFilename();
     }
 }
